93fb1aee4a1f4dc0bdc81657835638efb06aeb82
jsakamoto <junichi.jp@gmail.com>
2014-08-08 22:30:09 +0900

    Step 7 - Connect to Web API!

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 ﻿/// <reference path="scripts/angular.js" />
 /// <reference path="scripts/angular-resource.js" />
 
-var app = angular.module('SampleApp', []);
+var app = angular.module('SampleApp', ['ngResource']);
 
-app.controller('SampleController', function ($scope) {
-    $scope.bookmarks = [
-        { Title: "Twitter", URL: "https://twitter.com", Rating: 5 },
-        { Title: "facebook", URL: "https://facebook.com", Rating: 1 }
-    ];
+app.controller('SampleController', function ($scope, $resource) {
+
+    var webapi = $resource('http://code2014-angular-sampleapi.azurewebsites.net/api/bookmarks/:id', { id: '@Id' });
+    $scope.bookmarks = webapi.query();
 
     $scope.newbookmark = { Title: "", URL: "", Rating: 0 };
 
     $scope.addBookmark = function () {
         $scope.bookmarks.push($scope.newbookmark);
+        webapi.save($scope.newbookmark);
         $scope.newbookmark = { Title: "", URL: "", Rating: 0 };
     };
 
     $scope.removeBookmark = function (index) {
         if (!confirm('sure?')) return;
+        var bookmark = $scope.bookmarks[index];
         $scope.bookmarks.splice(index, 1);
+        bookmark.$remove();
     };
 });
