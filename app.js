cdfcec7c720cb9d923889c13fa7d86d149ce796c
jsakamoto <junichi.jp@gmail.com>
2014-08-08 21:36:41 +0900

    Step 4 - Feature of adding new item.

--- app.js
+++ app.js
@@ -3,4 +3,11 @@
         { Title: "Twitter", URL: "https://twitter.com", Rating: 5 },
         { Title: "facebook", URL: "https://facebook.com", Rating: 1 }
     ];
+
+    $scope.newbookmark = { Title: "", URL: "", Rating: 0 };
+
+    $scope.addBookmark = function () {
+        $scope.bookmarks.push($scope.newbookmark);
+        $scope.newbookmark = { Title: "", URL: "", Rating: 0 };
+    }
 }
